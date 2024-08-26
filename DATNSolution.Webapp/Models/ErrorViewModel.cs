e6777e8fe1eb402a2baac7bc817c4994a94c3e05
LAPTOP-T78D6ENU\nhuhoang <nhuhoang2001524@gmail.com>
2024-08-26 23:13:33 +0700

    Add project files.

---
+++ DATNSolution.Webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DATNSolution.Webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
